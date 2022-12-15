e95de25946985014970bf053cd2541dd6ea6f2da
David Mutuku <keithdave23@gmail.com>
2022-12-14 18:44:46 -0800

    lowercase alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,13 +7,18 @@
 
 /**
  * print alphabet in lowercase
+ *
+ * Return: void
  */
 void print_alphabet(void)
 {
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
+	{
 		_putchar(i);
+	}
+
 
 	_putchar('\n');
 }
