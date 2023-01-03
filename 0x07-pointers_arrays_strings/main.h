f8a97c86eaa2dbcb5b25f97806c4f8958e6b441a
David Mutuku <keithdave23@gmail.com>
2023-01-03 05:44:17 -0800

    add main

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * Desc: Header file containing prototypes for all functions
+ */
 
 #include <stddef.h>
-
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
