28b87414c59fb1455984893950ac39f7fa5dc66d
David Mutuku <keithdave23@gmail.com>
2023-01-03 17:34:50 -0800

    locates character

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -16,3 +16,4 @@ unsigned int _strspn(char *s, char *accept);
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+#endif /* MAIN_H */
