ce2436cb29edf987cc65c2cea951b3e290dcab68
David Mutuku <keithdave23@gmail.com>
2023-01-05 18:21:15 -0800

    recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+/**
+ * void prototypes(void)
+ * int prototypes(int)
+ * char prototypes(char)
+ */
+void _puts_recursion(char *s);
+
+#endif /* MAin_H */
