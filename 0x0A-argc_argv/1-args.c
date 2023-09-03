f253c9b393ef5f66a7418d957fb095dbf7d14380
davewrld <keithdave23@gmail.com>
2023-09-03 04:28:26 -0700

    number of arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ * @argc: argument count.
+ * @argv: Pointer to array.
+ *
+ * Return: 0 (succes).
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
