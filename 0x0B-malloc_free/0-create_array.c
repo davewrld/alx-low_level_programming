1da4669cc05ff709c61643818534b2a1e36f9b63
davewrld <keithdave23@gmail.com>
2023-09-04 03:09:29 -0700

    Memory management

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - of char and initialize it with a char.
+ * @size: of the array.
+ * @c: character to be initialized.
+ *
+ * Return: Pointer to created array else NULL if fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+		return (NULL);
+
+	char *array = malloc(size * sizeof(char));
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (unsigned int i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+
+	return (array);
+}
