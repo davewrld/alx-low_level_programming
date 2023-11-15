89819024f1ee27933e84c609d5419a116b759294
davewrld <keithdave23@gmail.com>
2023-11-15 07:30:30 -0800

    prints string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- * _puts_recursion - Prints a string and newline.
- * @s: pointer to string
+ * -puts_recursion - Prints a string and new line.
+ *  @s: pointer to string to be printed
  */
-void _print_rev_recursion(char *s)
+void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
