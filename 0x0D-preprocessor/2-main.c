788a6a6e246d96c8fe354b2f40e99320aa5e5a38
davewrld <keithdave23@gmail.com>
2023-11-27 12:59:00 -0800

    uodated

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point
+ *
+ * Description: Prints name of source file.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
 
