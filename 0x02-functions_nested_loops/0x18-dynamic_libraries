b8273713c8915a70e8e17326bc3bd5496c95511b
davewrld <keithdave23@gmail.com>
2024-01-30 23:05:27 -0800

    change 3 is lower

---
+++ 0x02-functions_nested_loops/0x18-dynamic_libraries
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercaase character.
+ *
+ * Return: 1  if lowercase else.
+ */
+int _islower(int c)
+	if return (1);
+	return (0);
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,1 @@
-#include "main.h"
 
-/**
- * _islower - Checks for lowercaase character.
- *
- * Return: 1  if lowercase else.
- */
-int _islower(int c)
-	if return (1);
-	return (0);
