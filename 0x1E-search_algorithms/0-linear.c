41d19bff7a759c83ee8d15a25daac2aadb89ae2e
davewrld <keithdave23@gmail.com>
2024-05-25 18:14:48 -0700

    0. Linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - Searches for a value using linear search.
+ * @array: pointer to array.
+ * @size: size of the array.
+ * @value: value to search for.
+ *
+ * Return: index of value.
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+    size_t i;
+
+    if (array == NULL)
+        return (-1);
+
+    for (i = 0; i < size; i++)
+    {
+            printf("value checked array[%lu] = [%d]\n", i, array[i]);
+        if(array[i] == value)
+        {
+            return (i);
+        }
+    }
+    return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,13 @@
+0x1E. C - Search Algorithms
+
+#OBJECTIVES
+What is a search algorithm
+What is a linear search
+What is a binary search
+What is the best search algorithm to use depending on your needs
+
+#Tasks
+0. Linear search - Function that searches for a value in an array of integers using the Linear search algorithm.
+1. Binary search - function that searches for a value in a sorted array of integers using the Binary search algorithm
+2. Big O #0- time complexity (worst case) of a linear search in an array of size n?
+3.
